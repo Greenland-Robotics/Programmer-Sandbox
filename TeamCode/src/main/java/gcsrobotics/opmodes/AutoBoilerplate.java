bd0cebf276f76545c47da16c0d56300089e7ce22
jkelley9 <josh9kelley@gmail.com>
2025-06-29 11:50:25 -0400

    Added boilerplate opmodes

---
+++ TeamCode/src/main/java/gcsrobotics/opmodes/AutoBoilerplate.java
@@ -0,0 +1,24 @@
+package gcsrobotics.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;// You won't need this
+
+import gcsrobotics.framework.AutoBase;
+
+@Autonomous(name="Auto Boilerplate")
+@Disabled
+//Do NOT include this. This is to ensure this program doesn't show up on the driver hub, but
+//you will want your programs to show up
+public class AutoBoilerplate extends AutoBase {
+
+    //Not necessary, but this is the code to run during init
+    @Override
+    protected void initSequence(){
+
+    }
+
+    @Override
+    protected void runSequence() {
+
+    }
+}
