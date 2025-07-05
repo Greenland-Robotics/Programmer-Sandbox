021952d164929c170d2d9935c91cfb52c2379486
jkelley9 <josh9kelley@gmail.com>
2025-07-04 20:46:48 -0400

    Fixed minor bug

--- TeamCode/src/main/java/gcsrobotics/framework/OpModeBase.java
+++ TeamCode/src/main/java/gcsrobotics/framework/OpModeBase.java
@@ -44,6 +44,7 @@ public abstract class OpModeBase extends LinearOpMode {
             telemetry.addData("Error",e.getMessage());
             telemetry.addLine("Hardware initialization failed");
             telemetry.update();
+            sleep(5000);
             stop();
         }
 
